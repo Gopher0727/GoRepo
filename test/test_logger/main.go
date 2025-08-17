e164e6ee34e466f788d5b8016110ff0a92738d42
Gopher <1900271083@qq.com>
2025-08-17 21:55:49 +0800

    feat: add test logger example

---
+++ test/test_logger/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"repo/pkg/logger"
+)
+
+func main() {
+	log.Println("日志系统已初始化，日志将写入 app.log 文件")
+	// 示例日志
+	logger.Trace.Println("这是一个跟踪信息")
+	logger.Info.Println("这是一个普通信息")
+	logger.Warning.Println("这是一个警告信息")
+	logger.Error.Println("这是一个错误信息")
+}
