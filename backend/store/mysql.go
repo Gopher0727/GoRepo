701b8dabd36845ab061140fcb82e27f4bb78b393
Gopher <1900271083@qq.com>
2025-09-14 13:37:05 +0800

    feat(store): add automatic database creation and connection tuning

--- backend/store/mysql.go
+++ backend/store/mysql.go
@@ -33,6 +33,22 @@ func InitMySQL(cfg models.MySQLConfig) (*gorm.DB, error) {
 		cfg.Database = "testdb"
 	}
 
+	{
+		rawDSN := fmt.Sprintf("%s:%s@tcp(%s:%d)/?charset=utf8mb4&parseTime=True&loc=Local", user, cfg.RootPassword, host, cfg.Port)
+		tmp, err := gorm.Open(mysql.Open(rawDSN), &gorm.Config{Logger: logger.Default.LogMode(logger.Warn)})
+		if err != nil {
+			return nil, err
+		}
+		sqlDB, err := tmp.DB()
+		if err != nil {
+			return nil, err
+		}
+		if _, err := sqlDB.Exec("CREATE DATABASE IF NOT EXISTS `" + cfg.Database + "` DEFAULT CHARACTER SET utf8mb4"); err != nil {
+			return nil, err
+		}
+		_ = sqlDB.Close()
+	}
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, cfg.RootPassword, host, cfg.Port, cfg.Database)
 
 	gormCfg := &gorm.Config{
